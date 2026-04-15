7c6fa74abe68efa27b9905e05753f5fb0141eff5
Joyce <joyfq23@gmail.com>
2026-04-15 14:28:43 -0400

    feat: creación de asteroides en movimiento

--- script.js
+++ script.js
@@ -10,13 +10,90 @@ let ship = {
 let keys = {};
 let velocidadRotacion = 0.05;
 let fuerza = 0.2;
+
+let asteroides = [];
+class Asteroide {
+    constructor(x, y, dx, dy, radio) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radio = radio;
+        this.vertices = this.generarVertices();
+    }
+
+    generarVertices() {
+        const puntos = Math.floor(Math.random() * 5) + 8; 
+        const vertices = [];
+        for (let i = 0; i < puntos; i++) {
+            const angulo = (i / puntos) * Math.PI * 2;
+            const variacion = this.radio * (0.6 + Math.random() * 0.4); 
+            vertices.push({
+                x: Math.cos(angulo) * variacion,
+                y: Math.sin(angulo) * variacion
+            });
+        }
+        return vertices;
+    }
+
+    mover() {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+
+    dibujar() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.beginPath();
+        ctx.moveTo(this.vertices[0].x, this.vertices[0].y);
+        for (let i = 1; i < this.vertices.length; i++) {
+            ctx.lineTo(this.vertices[i].x, this.vertices[i].y);
+        }
+        ctx.closePath();
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        ctx.restore();
+    }
+}
+
+function crearAsteroide() {
+    let lado = Math.floor(Math.random() * 4);
+    let x, y;
+
+    if (lado === 0) {
+        x = Math.random() * canvas.width;
+        y = 0;
+    } else if (lado === 1) {
+        x = Math.random() * canvas.width;
+        y = canvas.height;
+    } else if (lado === 2) {
+        x = 0;
+        y = Math.random() * canvas.height;
+    } else {
+        x = canvas.width;
+        y = Math.random() * canvas.height;
+    }
+
+    let radio = Math.random() * 40 + 30;
+
+    let anguloHaciaCentro = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+    let variacion = (Math.random() - 0.5) * 1;
+
+    let velocidadBase = Math.random() * 2 + 1;
+    let dx = Math.cos(anguloHaciaCentro + variacion) * velocidadBase;
+    let dy = Math.sin(anguloHaciaCentro + variacion) * velocidadBase;
+    return new Asteroide(x, y, dx, dy, radio);
+
+}
+
 window.addEventListener("keydown", (e) => {
     keys[e.key] = true;
 });
 
 window.addEventListener("keyup", (e) => {
     keys[e.key] = false;
-}); 
+});
 function drawShip() {
     ctx.save();
     ctx.translate(ship.x, ship.y);
@@ -34,14 +111,14 @@ function drawShip() {
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (keys["ArrowLeft"]) {
-    ship.angle -= velocidadRotacion;
+        ship.angle -= velocidadRotacion;
     }
     if (keys["ArrowRight"]) {
-    ship.angle += velocidadRotacion;
+        ship.angle += velocidadRotacion;
     }
     if (keys["ArrowUp"]) {
-    ship.vx += Math.cos(ship.angle) * fuerza;
-    ship.vy += Math.sin(ship.angle) * fuerza;
+        ship.vx += Math.cos(ship.angle) * fuerza;
+        ship.vy += Math.sin(ship.angle) * fuerza;
     }
     ship.x += ship.vx;
     ship.y += ship.vy;
@@ -51,6 +128,21 @@ function loop() {
     if (ship.x < 0) ship.x = canvas.width;
     if (ship.y > canvas.height) ship.y = 0;
     if (ship.y < 0) ship.y = canvas.height;
+
+    if (Math.random() < 0.005) {
+        asteroides.push(crearAsteroide());
+    }
+
+    asteroides.forEach((asteroide, i) => {
+        asteroide.mover();
+        asteroide.dibujar();
+        if (
+            asteroide.x < -100 || asteroide.x > canvas.width + 100 ||
+            asteroide.y < -100 || asteroide.y > canvas.height + 100
+        ) {
+            asteroides.splice(i, 1);
+        }
+    });
     drawShip();
     requestAnimationFrame(loop);
 }
